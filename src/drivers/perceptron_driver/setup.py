168b41247e4048789365d0cc5e22f36b18287911
Pedro Soares <pmbs.123@gmail.com>
2023-10-29 15:40:08 +0100

    build: add both launch and config folders to be included

--- src/drivers/perceptron_driver/setup.py
+++ src/drivers/perceptron_driver/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = "perceptron_driver"
 
@@ -12,6 +13,8 @@ setup(
             ["resource/" + package_name],
         ),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*_launch.py")),
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
