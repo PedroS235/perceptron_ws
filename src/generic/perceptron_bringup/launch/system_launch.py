a4121b19dfe492d802cd2bc79b850f336a237108
Pedro Soares <pmbs.123@gmail.com>
2024-02-13 11:25:40 +0100

    feat(generic): launch system for drivers

---
+++ src/generic/perceptron_bringup/launch/system_launch.py
@@ -0,0 +1,44 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def launch_perceptron_driver() -> IncludeLaunchDescription:
+    pkg_dir = get_package_share_directory("perceptron_bringup")
+    params_file = os.path.join(pkg_dir, "config", "params.yaml")
+    perceptron_driver_pkg_dir = get_package_share_directory(
+        "perceptron_driver"
+    )
+    return IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                perceptron_driver_pkg_dir, "launch/perceptron_driver_launch.py"
+            )
+        ),
+        launch_arguments={"params_file": params_file}.items(),
+    )
+
+
+def launch_lidar_driver() -> IncludeLaunchDescription:
+    pkg_dir = get_package_share_directory("unitree_bringup")
+    params_file = os.path.join(pkg_dir, "config", "params.yaml")
+    ldlidar_pkg_dir = get_package_share_directory("ldlidar_stl_ros2")
+    return IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ldlidar_pkg_dir, "launch/ld19.launch.py")
+        ),
+    )
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    ld.add_action(launch_perceptron_driver())
+    ld.add_action(launch_lidar_driver())
+
+    return ld
